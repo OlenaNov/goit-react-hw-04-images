c39d5eb8ea69274fd6b836f49d50a8b092823c8e
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-04-15 08:16:49 +0200

    start2

--- src/components/utilities/api.js
+++ src/components/utilities/api.js
@@ -12,7 +12,7 @@ export const fetchImages = async (query, page) => {
                 key: API_KEY,
                 image_type: 'photo',
                 orientation: 'horizontal',
-                per_page: 12,
+                per_page: 12
         }});
         return response.data.hits;
 };
