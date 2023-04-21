223cb458269b8c69c6fb3eab379e278610efa9fe
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-04-21 08:48:26 +0200

    finish base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { Component } from "react";
+import { useState } from "react";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import Layout from "./Layout";
 import Searchbar from "./Searchbar";
 import { fetchImages } from "./utilities/api";
@@ -8,72 +9,64 @@ import Loader from "./Loader";
 import { errors } from './constants/errors';
 import Error from "./Error";
 
-export class App extends Component {
+export function App() {
 
-  state = {
-    result: [],
-    query: '',
-    page: 1,
-    error: '',
-  };
+  const [result, setResult] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  handleSubmit = async query => {
-    this.setState({ isLoading: true, });
-
+  const handleSubmit = async query => {
+    setIsLoading(true);
     try {
 
     const result =  await fetchImages(query);
-    this.setState({ 
-        result: [...result],
-        query,
-        page: 1,
-       });
+    setResult([...result]);
+    setQuery(query);
+    setPage(1);
 
+    if(!result.length) {
+      Notify.info("Sorry, we didn't find the image you requested. Try again!");
+    };
       } catch {
-        this.setState({ error: errors.errFetch });
-
+        setError(errors.errFetch);
     } finally {
-      
-      this.setState({ isLoading: false, });
+      setIsLoading(false);
     };
   };
 
-  handleLoadMore = async () => {
-    this.setState({ isLoading: true, });
-    const { query, page } = this.state;
+  const handleLoadMore = async () => {
+    setIsLoading(true);
+
     const nextPage = page + 1;
     try {
     const result =  await fetchImages(query, nextPage);
 
-    this.setState(prevState => ({ 
-
-        result: [...prevState.result, ...result],
-        page: nextPage,
-       }));
+      if(!result.length) {
+        Notify.info("Sorry, we didn't find the image you requested. Try again!");
+      };
+      setResult(s => [...s, ...result]);
+      setPage(nextPage);
 
       } catch {
-        this.setState({ error: errors.errFetch });
-
+        setError(errors.errFetch);
     } finally {
-
-      this.setState({ isLoading: false, });
+      setIsLoading(false);
     };
-  }
-
-  render() {
-    const { result, isLoading, error } = this.state;
+  };
 
     return (
 
       <Layout>
-        <Searchbar onSubmit={this.handleSubmit} />
-        {result && <ImageGallery items={result} /> }
+        <Searchbar onSubmit={handleSubmit} />
+        {result
+        && <ImageGallery items={result} />}
         {isLoading && <Loader />}
         {result.length && !isLoading 
-        ? <Button fetchMore={this.handleLoadMore} /> 
+        ? <Button fetchMore={handleLoadMore} /> 
         : null}
         {error && <Error errorText={error} />}
       </Layout>
     );
-  };
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,9 +6,7 @@ const modalRoot = document.querySelector('#modal-root');
 
 export default function Modal({ onClose, children }) {
 
-    
     const handleKeyDown = useCallback(e => {
-
         if(e.code === 'Escape') {
             onClose();
         };
@@ -16,17 +14,8 @@ export default function Modal({ onClose, children }) {
 
     useEffect(() => {
         window.addEventListener('keydown', handleKeyDown);
-        return window.removeEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown);
     }, [handleKeyDown]);
-
-    // componentDidMount() {
-    //     window.addEventListener('keydown', this.handleKeyDown);
-    // };
-
-    // componentWillUnmount() {
-    //     window.removeEventListener('keydown', this.handleKeyDown);
-    // };
-
 
     const handleBackdrop = e => {
         if(e.currentTarget === e.target) {
